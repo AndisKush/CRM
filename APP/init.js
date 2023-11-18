150efa6a709026a3c723be78cb143fb26b68dee0
Anderson <anderson.ojunior@gmail.com>
2023-11-18 13:38:03 -0300

    main

--- APP/init.js
+++ APP/init.js
@@ -2,10 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// Lista de pastas e arquivos que você deseja incluir no cache
+const staticFolders = ['assets', 'favicon', 'produtos', 'logo.png'];
+
+// Configurando o middleware para servir arquivos estáticos com cache específico para cada item
+staticFolders.forEach(folderOrFile => {
+  const fullPath = path.join(__dirname, 'build', folderOrFile);
+  app.use(`/${folderOrFile}`, express.static(fullPath, {
+    maxAge: '1w', // Cache por uma semana
+  }));
+});
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
